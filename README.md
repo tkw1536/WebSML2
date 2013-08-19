0d1d0d44b0dc9475a22a498b1a0a0fc24168f69d
Tom Wiesing <tkw01536@gmail.com>
2013-08-19 08:55:44 +0200

    Switched to jQuery UI & added License

--- README.md
+++ README.md
@@ -2,14 +2,39 @@
 
 WebSML2 is an application which allows you to run SML from your web browser. 
 
-It is still a work in progress, so things might break. You can check out the demo [http://tkw1536.github.io/WebSML2](http://tkw1536.github.io/WebSML2). 
+It is still a work in progress, so things might break. You can check out the demo at [http://tkw1536.github.io/WebSML2](http://tkw1536.github.io/WebSML2). 
 
 It is heavily based on SMLtoJS and the SMLtoJS_prompt. 
 
 It uses the following libraries: 
 
-* [SMLtoJs](http://www.smlserver.org/smltojs/) 4.3.5
+* [SMLtoJs](http://www.smlserver.org/smltojs/) 4.3.5, licensed under MIT / GPLv2
 * [smlObfuscator](https://github.com/kpj/smlObfuscator)
-* [jQuery](http://jquery.com/) 1.10.2
-* [Bootstrap](http://getbootstrap.com/2.3.2/) 2.3.2
-* [jquery.nicescroll](http://areaaperta.com/nicescroll) 3.5.1
+* [jQuery](http://jquery.com/) 1.10.2, licensed under MIT
+* [jQuery UI](http://jqueryui.com/) 1.10.3, licensed under MIT
+* [jquery.nicescroll](http://areaaperta.com/nicescroll) 3.5.1, licensed under MIT
+
+# TODO
+
+* make a copy / save system
+* solve the main area design better
+* make tests in browsers and add a supported browsers page
+
+# License
+Licensed under GPLv2. See the file LICENSE for full license text. 
+
+	WebSML2 - Running SML online
+	Copyright (C) 2013  Tom Wiesing
+
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License 
+	version 2 as published by the Free Software Foundation
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
--- main/main.js
+++ main/main.js
@@ -1,18 +1,195 @@
-(function($){
+/*
+    WebSML2 - Running SML online
+    Copyright (C) 2013  Tom Wiesing
 
-	var init_run_ui, init_loader, output; 
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License 
+    version 2 as published by the Free Software Foundation
 
-	init_loader = function(){
-		$("#ui, #topbar").hide().removeClass("hidden");
-		$("#msgbox").center(); 
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
 
-		$(window).resize(function(){
-			$("#ui").height($(window).height()-50); 
-		}).resize(); 
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+jQuery(function($){
+
+	var Instance, 
+	output, 
+	compress_button, 
+	already_compressed, 
+	compress, 
+	compress_only, 
+	compress_close, 
+	compress_restore; 
+
+	//set the progressbar
+	$("#progressbar").progressbar({value: false});
+
+	//center the message box
+	$("#msgbox").center(); 
+
+	//hide the non-visible UI components
+	$("#ui, #topbar").hide(); 
+
+	//make the nice scroll uis
+	$("#code, #right, #compressed").niceScroll();
+
+	//Create dialogs
+	$("#about").dialog({
+		autoOpen: false, 
+		modal: true
+	}); 
+
+	//Create menu buttons
+	$("#runbtn").button({
+		icons: {
+			primary: "ui-icon-play"
+     	}
+	});
+
+	$("#compressbtn, #aboutbtn, #clearbtn").button(); 
+
+	//the compress button + dialog
+	$("#compressdialog").hide().dialog({
+		autoOpen: false, 
+		modal: true,
+		resize: false,
+		buttons: {
+			"Compress & Save": function(){
+				compress_close(); 
+			},
+			"Compress": function(){
+				compress_only(); 
+			},
+			"Restore": function(){
+				compress_restore(); 
+			},
+			"Cancel": function(){
+				$(this).dialog("close"); 
+			}
+		}
+	}); 
+
+	compress_button = $("#compressdialog").parent().find(".ui-button-text").eq(1); 
+
+	already_compressed = false; 
+
+	compress = function(){
+		try{
+			return SML($(already_compressed?"#compressed":"#code").val(), {
+				"exceptions": $("#ignore_funcs").val().split(" ").join("").split(","),
+				"dicGenerator": SML.dicGenerator[$("#replacer").val()],
+				"removeComments": $("#remove_comments").is(":checked"),
+				"simpleOptimize": $("#simple_optimize").is(":checked"),
+				"removeLineBreaks": $("#remove_lines").is(":checked"),
+				"preOptimize": function(code){
+					return ($("#add_garbage").is(":checked"))?SML.addRandomComments(code):code;
+				}
+			});
+		} catch(e){
+			return false; 
+		}
+
+		$("#compressed").val(res); 
+	};
+
+	compress_only = function(){
+		var compressed = compress(); 
+
+		$("#compressed").val(compressed?compressed:"(* Compress failed *)"); 
+
+		already_compressed = compress?true:false; 
+		compress_button.text(compress?"Save": "Compress & Save");
+	};
+
+
+
+	compress_close = function(){
+		var compressed = compress(); 
+		if(compressed){
+			$("#code").val(already_compressed?$("#compressed").val():compressed); 
+			$("#compressdialog").dialog("close"); 
+		} else {
+			$("#compressed").val("(* Compress failed *)");
+		}
 		
+	};
+
+	compress_restore = function(){
+		already_compressed = false; 
+		compress_button.text("Compress & Save");
+		$("#compressed").val($("#code").val()); 
+	};
+
+	$("#compressbtn").click(function(){
+		compress_restore(); 
+		$("#compressdialog")
+		.dialog("open"); 
+	});
+
+	//the about button + dialog
+	$("#aboutdialog").find("a").button(); //create buttons
+
+	$("#aboutdialog").hide().dialog({
+		autoOpen: false, 
+		modal: true,
+		resize: false,
+		buttons: {
+			"Close": function(){
+				$(this).dialog("close"); 
+			}
+		}
+	}); 
+
+	$("#aboutbtn").click(function(){
+		$("#aboutdialog").dialog("open"); 
+	});
+
+	// the clear button
+
+	$("#clearbtn").click(function(){
+		$("#right").empty(); 
+
+		output("Output cleared. ").addClass("label label-clear");
+	});
+
+	//the run button
+	$("#runbtn").click(function(){
+		output("Running new program").addClass("label label-new"); 
+
+		Interpreter.run($("#code").val(), function(allText, state){
+			
+			if(state == 0){
+				output("Program finished").addClass("label label-finish"); 
+			} else {
+				output("Program failed").addClass("label label-fail"); 
+			}
+		}); 
+	});
+
+	//bind resize hacks
+	$(window).resize(function(){
+			$("#ui").height($(window).height()-$("#topbar").height()-20); 
+
+			$("#compressdialog, #aboutdialog")
+			.dialog("option", "width", 0.8*$(window).width())
+			.dialog("option", "height", 0.8*$(window).height());
+	}).resize(); 
+
+	
+
+
+	//start loading after showing the license text for 1 second
+	window.setTimeout(function(){
+		$("#progressbar > span").remove(); //remove the license stuff
 
 		runLoader(function(percent, line1, line2){
-			$(".bar").width(Math.round(percent)+"%");
+			$("#progressbar").progressbar({value: Math.round(percent)}); 
 			$("#msgline1").text(line1); 
 			$("#msgline2").text(line2);
 			$("#msgbox").center();  
@@ -21,104 +198,25 @@
 				$(this).remove();
 			}); 
 
-			init_run_ui(); 
+			$("#ui, #topbar") //fade these in
+			.fadeIn(1000); 
+
+
+			//set the interpreter
+			Interpreter = window.Interpreter = new SMLC(); 
+			Interpreter.on("output", function(e, chunk, italic){
+				output(chunk, true);
+			});
+
 		});
-	}; 
-	
-
-	init_run_ui = function(){
-		$("#ui, #topbar")
-		.fadeIn(1000); 
-
-		$("#code, #right, #compressed").niceScroll();
-
-		$("#clear").click(function(){
-			$('#right')
-			.html("")
-			.append(
-			'<span class="label label-info">Output cleared. </span><br>'
-			); 
-		}); 
-
-		var Interpreter = window.Interpreter = new SMLC(); 
-
-		Interpreter.on("output", function(e, chunk, italic){
-			output(chunk, true);
-		})
-
-		$("#run").click(function(){
-			output("Running new program").addClass("label label-success"); 
-
-			Interpreter.run($("#code").val(), function(allText, state){
-				
-				if(state == 0){
-					output("Program finished").addClass("label label-success"); 
-				} else {
-					output("Program failed").addClass("label label-important"); 
-				}
-			}); 
-		});
-
-		var already_compressed = false; 
-
-		var compress = function(){
-			try{
-				return SML($(already_compressed?"#compressed":"#code").val(), {
-					"exceptions": $("#ignore_funcs").val().split(" ").join("").split(","),
-					"dicGenerator": SML.dicGenerator[$("#replacer").val()],
-					"removeComments": $("#remove_comments").is(":checked"),
-					"simpleOptimize": $("#simple_optimize").is(":checked"),
-					"removeLineBreaks": $("#remove_lines").is(":checked"),
-					"preOptimize": function(code){
-						return ($("#add_garbage").is(":checked"))?SML.addRandomComments(code):code;
-					}
-				});
-			} catch(e){
-				return false; 
-			}
-
-			$("#compressed").val(res); 
-		}
-
-		$("#compress_only").click(function(){
-			var compressed = compress(); 
-
-			$("#compressed").val(compressed?compressed:"(* Compress failed *)"); 
-
-			already_compressed = compress?true:false; 
-			$("#compress_close").text(compress?"Save": "Compress & Save");
-		});
-
-		$("#compress_close").click(function(){
-			var compressed = compress(); 
-			if(compressed){
-				$("#code").val(already_compressed?$("#compressed").val():compressed); 
-				$("#compressdialog").modal("hide"); 
-			} else {
-				$("#compressed").val("(* Compress failed *)");
-			}
-			
-		});
-
-		$("#restore_code").click(function(){
-			already_compressed = false; 
-			$("#compress_close").text("Compress & Save");
-			$("#compressed").val($("#code").val()); 
-		})
-
-		$("#compressdialog").on("shown", function(){
-			$("#restore_code").click(); 
-		});
-	};
+	}, 1000); 
 
 
-	output = function(chunk, br){
+	output = function(chunk){
 		var res = $("<span>").text(chunk).appendTo("#right"); 
 		$(" <br> ").appendTo("#right");
 
 		$('#right').scrollTop($('#right')[0].scrollHeight);
 		return res; 
 	}
-
-	$(init_loader); 
-})(jQuery); 
+}); 
